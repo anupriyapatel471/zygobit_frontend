94a1dfee3045c5032931970af7d230fb62c50bb6
Gouravdeep <gouravdeepsingh@gmail.com>
2025-03-27 19:33:32 +0530

    content

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,12 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//     images: {
+//       domains: ['images.unsplash.com', 'pbs.twimg.com', "assets.aceternity.com"],
+//     },
+//   };
+
+//   export default nextConfig;
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
