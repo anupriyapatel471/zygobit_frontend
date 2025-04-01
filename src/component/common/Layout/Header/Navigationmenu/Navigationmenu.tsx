701968f11dc96c2eb8f4b99eee9fa54383af538a
Gouravdeep <gouravdeepsingh@gmail.com>
2025-04-01 10:35:33 +0530

    navigation issue

--- src/component/common/Layout/Header/Navigationmenu/Navigationmenu.tsx
+++ src/component/common/Layout/Header/Navigationmenu/Navigationmenu.tsx
@@ -4,7 +4,6 @@ import * as React from "react";
 import Link from "next/link";
 
 import { cn } from "@/lib/utils";
-// import { Icons } from "@/components/icons"
 import {
   NavigationMenu,
   NavigationMenuContent,
@@ -21,58 +20,115 @@ export default function NavigationMenuDemo() {
   const handleClassChange = (val: string) => {
     setClassDynamic(val);
   };
+
   return (
     <NavigationMenu className={classDynamic}>
       <NavigationMenuList className="grid grid-cols-1 gap-4 space-x-0 lg:space-x-1 lg:flex">
         <NavigationMenuItem
           onClick={() => handleClassChange(" top-6 lg:top-full")}
-          className="  text-white"
+          className="text-white"
         >
           <NavigationMenuTrigger>Services</NavigationMenuTrigger>
           <NavigationMenuContent>
             <ul className="grid grid-cols-1 gap-3 p-4 sm:p-6 items-center w-full md:w-[400px] lg:w-[900px] lg:grid-cols-3">
               <div className="w-auto grid grid-cols-1 lg:grid-cols-2 gap-3 col-span-2">
-                <ListItem
-                  className="text-white hover:text-white hover:bg-gradient-to-b from-accent-foreground/50 to-accent-foreground"
-                  href="/services/mobile-app-development"
-                  title="Mobile App Development"
-                >
-                  Create high-performance mobile apps for iOS and Android.
-                </ListItem>
-                <ListItem
-                  className="text-white hover:text-white hover:bg-gradient-to-b from-accent-foreground/50 to-accent-foreground"
-                  href="/services/web-app-development"
-                  title="Web Development"
-                >
-                  Develop fast, responsive, and optimized websites.
-                </ListItem>
-                <ListItem
-                  className="text-white hover:text-white hover:bg-gradient-to-b from-accent-foreground/50 to-accent-foreground"
-                  href="/services/uiux-web-design"
-                  title="UI/UX Design"
-                >
-                  Craft intuitive and engaging user experiences.
-                </ListItem>
-                <ListItem
-                  className="text-white hover:text-white hover:bg-gradient-to-b from-accent-foreground/50 to-accent-foreground"
-                  href="/services/machine-learning-ai"
-                  title="Machine Learning and AI"
-                >
-                  Leverage AI for automation, insights, and smarter solutions.
-                </ListItem>
-                <ListItem
-                  className="text-white hover:text-white hover:bg-gradient-to-b from-accent-foreground/50 to-accent-foreground"
-                  href="/services/Ecommerce"
-                  title="E-Commerce"
-                >
-                  Build secure online stores with easy payments.
-                </ListItem>
+                <li>
+                  <NavigationMenuLink asChild>
+                    <Link
+                      href="/services/mobile-app-development"
+                      className={cn(
+                        "block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground",
+                        "text-white hover:text-white hover:bg-gradient-to-b from-accent-foreground/50 to-accent-foreground"
+                      )}
+                    >
+                      <div className="text-sm font-medium leading-none">
+                        Mobile App Development
+                      </div>
+                      <p className="line-clamp-2 text-sm leading-snug text-muted-foreground">
+                        Create high-performance mobile apps for iOS and Android.
+                      </p>
+                    </Link>
+                  </NavigationMenuLink>
+                </li>
+                <li>
+                  <NavigationMenuLink asChild>
+                    <Link
+                      href="/services/web-app-development"
+                      className={cn(
+                        "block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground",
+                        "text-white hover:text-white hover:bg-gradient-to-b from-accent-foreground/50 to-accent-foreground"
+                      )}
+                    >
+                      <div className="text-sm font-medium leading-none">
+                        Web Development
+                      </div>
+                      <p className="line-clamp-2 text-sm leading-snug text-muted-foreground">
+                        Develop fast, responsive, and optimized websites.
+                      </p>
+                    </Link>
+                  </NavigationMenuLink>
+                </li>
+                <li>
+                  <NavigationMenuLink asChild>
+                    <Link
+                      href="/services/uiux-web-design"
+                      className={cn(
+                        "block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground",
+                        "text-white hover:text-white hover:bg-gradient-to-b from-accent-foreground/50 to-accent-foreground"
+                      )}
+                    >
+                      <div className="text-sm font-medium leading-none">
+                        UI/UX Design
+                      </div>
+                      <p className="line-clamp-2 text-sm leading-snug text-muted-foreground">
+                        Craft intuitive and engaging user experiences.
+                      </p>
+                    </Link>
+                  </NavigationMenuLink>
+                </li>
+                <li>
+                  <NavigationMenuLink asChild>
+                    <Link
+                      href="/services/machine-learning-ai"
+                      className={cn(
+                        "block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground",
+                        "text-white hover:text-white hover:bg-gradient-to-b from-accent-foreground/50 to-accent-foreground"
+                      )}
+                    >
+                      <div className="text-sm font-medium leading-none">
+                        Machine Learning and AI
+                      </div>
+                      <p className="line-clamp-2 text-sm leading-snug text-muted-foreground">
+                        Leverage AI for automation, insights, and smarter
+                        solutions.
+                      </p>
+                    </Link>
+                  </NavigationMenuLink>
+                </li>
+                <li>
+                  <NavigationMenuLink asChild>
+                    <Link
+                      href="/services/Ecommerce"
+                      className={cn(
+                        "block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground",
+                        "text-white hover:text-white hover:bg-gradient-to-b from-accent-foreground/50 to-accent-foreground"
+                      )}
+                    >
+                      <div className="text-sm font-medium leading-none">
+                        E-Commerce
+                      </div>
+                      <p className="line-clamp-2 text-sm leading-snug text-muted-foreground">
+                        Build secure online stores with easy payments.
+                      </p>
+                    </Link>
+                  </NavigationMenuLink>
+                </li>
               </div>
               <li className="row-span-1 h-full">
                 <NavigationMenuLink asChild>
                   <Link
-                    className="flex h-full w-full select-none flex-col justify-center items-center rounded-md bg-gradient-to-b from-accent-foreground/50 to-accent-foreground p-3 sm:p-6 no-underline outline-none focus:shadow-md"
                     href="/"
+                    className="flex h-full w-full select-none flex-col justify-center items-center rounded-md bg-gradient-to-b from-accent-foreground/50 to-accent-foreground p-3 sm:p-6 no-underline outline-none focus:shadow-md"
                   >
                     <div className="mb-2 mt-0 sm:mt-4 text-lg text-white font-medium">
                       Zygobit Tech
@@ -81,7 +137,7 @@ export default function NavigationMenuDemo() {
                       Scalable and feature-rich ecommerce platforms designed to
                       enhance business growth, ensure robust security,
                       streamline operations, and provide a seamless shopping
-                      experience for users.{" "}
+                      experience for users.
                     </p>
                   </Link>
                 </NavigationMenuLink>
@@ -107,30 +163,3 @@ export default function NavigationMenuDemo() {
     </NavigationMenu>
   );
 }
-
-const ListItem = React.forwardRef<
-  React.ElementRef<typeof Link>,
-  React.ComponentPropsWithoutRef<typeof Link>
->(({ className, title, children, href, ...props }, ref) => {
-  return (
-    <li>
-      <NavigationMenuLink asChild>
-        <Link
-          ref={ref}
-          href={href as string}
-          className={cn(
-            "block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground",
-            className
-          )}
-          {...props}
-        >
-          <div className="text-sm font-medium leading-none">{title}</div>
-          <p className="line-clamp-2 text-sm leading-snug text-muted-foreground">
-            {children}
-          </p>
-        </Link>
-      </NavigationMenuLink>
-    </li>
-  );
-});
-ListItem.displayName = "ListItem";
--- src/component/sevices/MachineLearning/AwareHoverCards/AwareHoverCards.tsx
+++ src/component/sevices/MachineLearning/AwareHoverCards/AwareHoverCards.tsx
@@ -4,8 +4,6 @@ import { DirectionAwareHover } from "../../../../components/ui/direction-aware-h
 import cardsData from "../../../../utils/content/AwareHoverCards/mlAi.json";
 
 export function AwareHoverCards() {
-  const imageUrl =
-    "https://images.unsplash.com/photo-1663765970236-f2acfde22237?q=80&w=3542&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
   return (
     <>
       <div className="w-full grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 lg:gap-6">
@@ -14,7 +12,7 @@ export function AwareHoverCards() {
             key={i}
             className="w-full h-auto relative  flex items-center justify-center"
           >
-            <DirectionAwareHover imageUrl={imageUrl}>
+            <DirectionAwareHover imageUrl={card.image}>
               <div className="w-full p-4 bg-[#D9D9D91A] backdrop-blur-xl rounded-b-xl border-t-0 border border-white ">
                 <h3 className="font-semibold text-xl mb-3">{card.title}</h3>
                 <p className="font-normal text-sm sm:text-base">
