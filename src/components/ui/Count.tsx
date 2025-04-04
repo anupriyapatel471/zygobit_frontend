c4a8e6657771a16b0f5aeec5193d9add794dea28
Gouravdeep <gouravdeepsingh@gmail.com>
2025-04-04 19:51:02 +0530

    fix count issue

--- src/components/ui/Count.tsx
+++ src/components/ui/Count.tsx
@@ -3,21 +3,22 @@
 import React, { useState, useEffect } from "react";
 
 const Count = ({ endValue }: any) => {
+  const numericEndValue = parseInt(endValue, 10);
   const [count, setCount] = useState(0);
 
   useEffect(() => {
     let start = 0;
     const duration = 2000;
-    const incrementTime = Math.ceil(duration / endValue);
+    const incrementTime = Math.ceil(duration / numericEndValue);
     const interval = setInterval(() => {
       start += 1;
       setCount(start);
-      if (start >= endValue) {
+      if (start >= numericEndValue) {
         clearInterval(interval);
       }
     }, incrementTime);
     return () => clearInterval(interval);
-  }, [endValue]);
+  }, [numericEndValue]);
 
   return <span>{count}</span>;
 };
