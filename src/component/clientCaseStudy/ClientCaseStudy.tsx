9747235f510827e724ba372e52e0474a3cb74fde
Anupriya patel <gouravdeepsingh@gmail.com>
2025-05-20 17:23:46 +0530

    portfolio design

--- src/component/clientCaseStudy/ClientCaseStudy.tsx
+++ src/component/clientCaseStudy/ClientCaseStudy.tsx
@@ -338,6 +338,7 @@ const ClientCaseStudy = ({ projectData }: { projectData: ProjectData }) => {
                       alt="icon"
                     />
                   </div>
+
                   <p className="text-sm lg:text-base">
                     {projectData?.developmentDescription}
                   </p>
