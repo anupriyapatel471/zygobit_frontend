e8848cb86fe1d913d12d45da2cf40d31851794d5
Anupriya patel <gouravdeepsingh@gmail.com>
2025-05-23 10:52:55 +0530

    image change

--- src/component/blog/TracingBeam/TracingBeam.tsx
+++ src/component/blog/TracingBeam/TracingBeam.tsx
@@ -1,8 +1,6 @@
-"use client";
 import React from "react";
 import Image from "next/image";
 import { TracingBeam } from "../../../components/ui/tracing-beam";
-import calendarIcon from "../../../../public/images/calendar_icon.svg";
 
 interface Blog {
   id: string;
@@ -20,55 +18,60 @@ interface Blog {
 }
 
 export function TracingBeams({ blog }: { blog: Blog }) {
+  const calendarIcon = "/images/calendar_icon.svg";
+
   return (
     <>
-      {blog && ( <TracingBeam className="mt-24 sm:mt-32 mb-16 sm:mb-20 px-0 sm:px-0 lg:px-0 max-w-[1248px] mx-auto">
-        <div className="container remove-bg">
-          <div className="w-full antialiased sm:pt-4 relative lg:pl-[60px]">
-            <div className="mb-10">
-              <h2 className=" text-white font-medium sm:font-bold text-2xl sm:text-4xl w-fit mb-2  sm:mb-4">
-                {blog.title}
-              </h2>
-              <b className="font-light sm:font-semibold text-sm block my-3">
-                Tag: {blog.tags && blog.tags.join(", ")}
-              </b>
-              <span className="flex items-center gap-2 text-xs ">
-                <Image
-                  className=" object-cover"
-                  src={calendarIcon}
-                  alt="icon"
-                  width={18}
-                  height={18}
-                />
-                Published on {blog.publishedDate}
-              </span>
+      {blog && (
+        <TracingBeam className="mt-24 sm:mt-32 mb-16 sm:mb-20 px-0 sm:px-0 lg:px-0 max-w-[1248px] mx-auto">
+          <div className="container remove-bg">
+            <div className="w-full antialiased sm:pt-4 relative lg:pl-[60px]">
+              <div className="mb-10">
+                <h2 className=" text-white font-medium sm:font-bold text-2xl sm:text-4xl w-fit mb-2  sm:mb-4">
+                  {blog.title}
+                </h2>
+                <b className="font-light sm:font-semibold text-sm block my-3">
+                  Tag: {blog.tags && blog.tags.join(", ")}
+                </b>
+                <span className="flex items-center gap-2 text-xs ">
+                  <Image
+                    className=" object-cover"
+                    src={calendarIcon}
+                    alt="icon"
+                    width={18}
+                    height={18}
+                  />
+                  Published on {blog.publishedDate}
+                </span>
 
-              <div className="text-sm sm:text-lg lg:text-xl  prose prose-sm dark:prose-invert">
-                <Image
-                  src={blog.image}
-                  alt="blog thumbnail"
-                  height="1000"
-                  width="1000"
-                  className="w-full rounded-2xl my-5 lg:my-9 object-cover"
-                />
-                {blog.description}
+                <div className="text-sm sm:text-lg lg:text-xl  prose prose-sm dark:prose-invert">
+                  <Image
+                    src={blog.image}
+                    alt="blog thumbnail"
+                    height="1000"
+                    width="1000"
+                    className="w-full rounded-2xl my-5 lg:my-9 object-cover"
+                  />
+                  {blog.description}
+                </div>
+                <p className="text-sm sm:text-lg lg:text-xl">{blog.title}</p>
+                {Array.isArray(blog.projectInfo) &&
+                  blog.projectInfo.length > 0 && (
+                    <div className="mt-6">
+                      {blog.projectInfo.map((project, projIndex) => (
+                        <div key={`project-${projIndex}`} className="mb-4">
+                          <h3 className="text-base sm:text-lg font-medium mb-2 sm:mb-0 sm:font-semibold">
+                            {project.SubTitle}
+                          </h3>
+                          <p className="text-sm font-light sm:font-normal">
+                            {project.SubDescription}
+                          </p>
+                        </div>
+                      ))}
+                    </div>
+                  )}
               </div>
-              <p className="text-sm sm:text-lg lg:text-xl">{blog.title}</p>
-              {Array.isArray(blog.projectInfo) &&
-                blog.projectInfo.length > 0 && (
-                  <div className="mt-6">
-                    {blog.projectInfo.map((project, projIndex) => (
-                      <div key={`project-${projIndex}`} className="mb-4">
-                        <h3 className="text-base sm:text-lg font-medium mb-2 sm:mb-0 sm:font-semibold">
-                          {project.SubTitle}
-                        </h3>
-                        <p className="text-sm font-light sm:font-normal">{project.SubDescription}</p>
-                      </div>
-                    ))}
-                  </div>
-                )}
             </div>
-          </div>
           </div>
         </TracingBeam>
       )}
