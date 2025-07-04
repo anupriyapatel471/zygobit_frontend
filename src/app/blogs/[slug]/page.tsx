5215f475e40ec0311eb24b5bc59ffd1e8164f78f
Anupriya patel <gouravdeepsingh@gmail.com>
2025-07-04 19:00:23 +0530

    metadata added

--- src/app/blogs/[slug]/page.tsx
+++ src/app/blogs/[slug]/page.tsx
@@ -47,9 +47,8 @@ export async function generateMetadata({
   params: { slug: string };
 }) {
   const blog = await fetchBlogBySlug(params.slug);
-
-  const title = blog?.title || DEFAULT_META.title;
-  const description = blog?.description || DEFAULT_META.description;
+  const title = blog?.metadata?.title || DEFAULT_META.title;
+  const description = blog?.metadata?.description || DEFAULT_META.description;
 
   return {
     title,
