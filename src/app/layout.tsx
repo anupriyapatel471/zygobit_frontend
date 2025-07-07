050ab3f67f18942a6f91d903de830805cb7e34f8
Anupriya patel <gouravdeepsingh@gmail.com>
2025-07-07 19:11:20 +0530

    layout scalable no

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -73,7 +73,7 @@ export default function RootLayout({
         />
         <meta
           name="viewport"
-          content="width=device-width, initial-scale=1.0, maximum-scale=1"
+          content="width=device-width, initial-scale=1.0, user-scalable=no"
         />
 
         <link
