f47b8e3c2a246093f31836a6b70260d79d0df8bd
Anupriya patel <gouravdeepsingh@gmail.com>
2025-09-13 12:58:54 +0530

    image change

--- src/component/clientCaseStudy/ClientCaseStudy.tsx
+++ src/component/clientCaseStudy/ClientCaseStudy.tsx
@@ -237,7 +237,6 @@ const ClientCaseStudy = ({ projectData }: { projectData: ProjectData }) => {
 
                 <div className="w-full order-1 sm:order-none sm:w-1/2 lg:w-2/5 relative">
                   <div className="hidden sm:inline absolute -top-20  -right-[120px] sm:-right-[220px] -z-10 w-[200px] h-[200px] sm:w-[400px] sm:h-[400px] rounded-full bg-[#FFC3A466]"></div>
-
                   <Image
                     width={520}
                     height={400}
